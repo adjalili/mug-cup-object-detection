29ae6cee5c8a25bc4282679ec02f5ea494188c9b
MD OLI AHMED <sa22240@student.samk.fi>
2026-03-19 11:24:09 +0200

    d

--- data/data_preparation.py
+++ data/data_preparation.py
@@ -5,6 +5,6 @@ from sklearn.model_selection import train_test_split
 
 dataset = pd.read_csv("data/CSVs/dataset.csv")
 
-train_data, val_data = train_test_split(*arrays: dataset, test_size=0.2)
+train_data, val_data = train_test_split(dataset, test_size=0.2)
 train_data.to_csv("data/CSVs/train.csv", index=False)
 val_data.to_csv("data/CSVs/val.csv", index=False)
