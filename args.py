0e277965d2347fedaf1851d0285d8ac222faf083
MD OLI AHMED <sa22240@student.samk.fi>
2026-05-01 13:45:29 +0300

    Fix training + detection pipeline

--- args.py
+++ args.py
@@ -1,6 +1,5 @@
 import argparse
 
-from dateutil import parser
 
 
 
@@ -10,16 +9,16 @@ def get_args():
     parser.add_argument('--backbone', type=str, default='FasterRCNN_resnet50_fpn',
                         choices=['FasterRCNN_resnet50_fpn'])
     
-    parser.add_argument('--num_classes', type=int, default=1)
+    parser.add_argument('--num_classes', type=int, default=2)
     parser.add_argument('--img_size', type=int, default=224)
 
     parser.add_argument('--csv_dir', type=str, default='data/CSVs')
     parser.add_argument('--output_dir', type=str, default='./sessions')
 
-    parser.add_argument('--batch_size', type=int, default=8,
+    parser.add_argument('--batch_size', type=int, default=4,
                         choices= [4, 8, 16, 32, 64])
     
-    parser.add_argument('--num_epochs', type=int, default=15)
+    parser.add_argument('--num_epochs', type=int, default=10)
     parser.add_argument('--lr', type=float, default=0.001)
     parser.add_argument('--wd', type=float, default=1e-4)
 
